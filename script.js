7109a44640dcd60a70a97037d16df53221f1f993
David Tabares <alejotabares1902@gmail.com>
2026-03-16 11:13:10 -0500

    ajustes al css y terminado del modulo limpiar

--- script.js
+++ script.js
@@ -1,36 +1,67 @@
 /*Diosito llevame */
-document.querySelector('btnCalcular').addEventListener('click', calcularRiesgo);
+document.querySelector('#btnCalcular').addEventListener("click", calcularRiesgo);
+document.querySelector('#btnLimpiar').addEventListener("click", limpiarinput);
 
+function calcularRiesgo() {
+    const edad = parseInt(document.getElementById('edad').value);
+    const presion = parseInt(document.getElementById('presion').value);
+    const colesterol = parseInt(document.getElementById('colesterol').value);
+    const fumar = document.getElementById('fumador').value;
 
-function calcularRiesgo(){
-    const edad =parseInt(document.getElementById('edad'));
-    const presion =parseInt(document.getElementById('presion'));
-    const colesterol =parseInt(document.getElementById('colesterol'));
-    const fumar =document.getElementById('fumador')
+    let riesgo = 0;
+    let valoracion = '';
+    if (!edad) {
+        document.getElementById('textoResultado').textContent = 'No hay datos en edad';
+    }
+    else if (!presion) {
+        document.getElementById('textoResultado').textContent = 'No hay datos en presión sistólica';
+    }
+    else if (!colesterol) {
+        document.getElementById('textoResultado').textContent = 'No hay datos en colesterol';
+    }else if(!fumar){
+        document.getElementById('textoResultado').textContent = 'No seleccionó un dato en Fumar';
 
-    let riesgo  = 0
-    if(edad >35){
-        riesgo += 0;
-    }else if(34>edad>45){
-        riesgo +=1;
-    }else if(edad>44){
-        riesgo +=2;
+    }
+    else {
+        if (edad > 35) {
+            riesgo += 0;
+        } else if (34 > edad > 45) {
+            riesgo += 1;
+        } else if (edad > 44) {
+            riesgo += 2;
+        }
+
+        if (presion >= 140) {
+            riesgo += 3;
+        } else if (presion <= 120) {
+            riesgo += 1;
+        }
+
+        if (colesterol >= 240) {
+            riesgo += 3;
+        } else if (colesterol >= 200) {
+            riesgo += 1;
+        } else {
+            riesgo += 0
+        }
+
+        if (fumar == 'si') {
+            riesgo += 3;
+        }
+
+        if (riesgo <= 2) {
+            valoracion = 'Bajo riesgo'
+            document.body.style.backgroundColor = "#00C201"
+            document.getElementById('textoResultado').textContent = valoracion
+        }
     }
 
-    if(presion >= 140){
-        riesgo +=3;
-    }else if(presion <=120){
-        riesgo +=1;
-    }
+}
 
-    if (colesterol >= 240){
-        riesgo +=3;
-    }else if(colesterol >= 200)
-        riesgo +=1;
 
-    if(fumar == 'si'){
-        riesgo +=3;
-    }
-
-    return riesgo
+function limpiarinput() {
+    document.getElementById('edad').value = '';
+    document.getElementById('presion').value ='';
+    document.getElementById('colesterol').value ='';
+    document.getElementById('fumador').value='';
 }
